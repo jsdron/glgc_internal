07e23e01329797c5ad2d841a685b4cbedd392469
Jacqueline S. Dron <13053565+jsdron@users.noreply.github.com>
2023-05-17 09:15:34 -0400

    Updated software requirements

--- src/0.2_install-dependancies.sh
+++ src/0.2_install-dependancies.sh
@@ -1,4 +1,10 @@
-bcftools
-tabix
-plink
+## noting these are the versions from the last GLGC iteration...i assume that they can be updated to their newest veresion
+
+BGZIP and tabix (0.2.6): http://samtools.sourceforge.net/tabix.shtml
+Bcftools (1.3.1): http://www.htslib.org/download
 qctools
+Vcftools (0.1.8): https://github.com/vcftools/vcftools
+PLINK1.9 (Plink2): https://www.cog-genomics.org/plink2
+PLINK 2.0
+GEM (1.4.5 or up): https://github.com/large-scale-gxe-methods/GEM 
+MAGEE
