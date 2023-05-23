aba7b2e15f35bdbbcc0c8b66382cf24c1b4141ea
Jacqueline S. Dron <13053565+jsdron@users.noreply.github.com>
2023-05-23 15:55:56 -0400

    Script updates + Google Sheet template
    Made script updates. Also created a Google Sheet template (.xlsx) that matches the GLGC Meta Data Google Sheet

--- src/0.1_directory-prep.sh
+++ src/0.1_directory-prep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ../data
-mkdir -p ../results_for_upload
+mkdir -p ../results_for_upload/imputation_qc/
+mkdir -p ../results_for_upload/pre_imputation_qc/
 mkdir -p ../tools
 mkdir -p ../results_tmp
--- src/4.3.1_perform-GWIS_GEM.sh
+++ src/4.3.1_perform-GWIS_GEM.sh
@@ -19,7 +19,7 @@
 ##									'BGEN' = .bgen input																		##
 ##									'PGEN' = .pgen, .pvar, .psam input															##
 ##									'BED'  = .bed, .bim, .fam																	##	
-##			*B (Type: String) = Path to imputation files, including the file prefix. do not include any file extensions. 		##
+##			*B (Type: String) = Path to imputation files, including the file prefix. Do not include any file extensions. 		##
 ##			*C (Type: String) = If 'BGEN' input does not contain sample identifiers, this is the path to the sample file, 		##
 ##								including the file prefix and extension. If the sample file is not needed, 						##
 ##								set parameter as 'NA'.  																		##
@@ -45,11 +45,11 @@
 ## 																																##
 ## ---------------------------------------------------------------------------------------------------------------------------- ##
 ## 	Example 1: 																													##	
-## 			4.3.1_perform-GWIS_GEM.sh 'BGEN' /path/to/my/bgen/fileName.bgen /path/to/my/bgen/sample/fileName.tsv				##
+## 			4.3.1_perform-GWIS_GEM.sh 'BGEN' /path/to/my/bgen/fileName /path/to/my/bgen/sample/fileName.tsv				##
 ##					/path/to/my/phenotype/fileName.tsv '\t' 'sample_ID' 'LDLC' 'bmi' 'age age2 sex PC1 PC2 PC3 PC3 PC4'			##	
 ##					'HDLC_ALLFAST_BMI_All_TOT_adult'
 ## 	Example 2: 																													##	
-## 			4.3.1_perform-GWIS_GEM.sh 'BGEN' /path/to/my/bgen/fileName.bgen NA /path/to/my/phenotype/fileName.csv ',' 'IID'
+## 			4.3.1_perform-GWIS_GEM.sh 'BGEN' /path/to/my/bgen/fileName NA /path/to/my/phenotype/fileName.csv ',' 'IID'
 ##					'LDLC' 'age' 'age2 sex PC1 PC2 PC3 PC3 PC4 PC5 PC6'	'LDLC_NONFAST_BMI_M_EAS_adult'
 
 ## 	Example 3: 																													##	
@@ -59,9 +59,9 @@
 ##################################################################################################################################
 
 # ------------------------------------- #
-#  Input parameters						#
+#  Input parameters											#
 # ------------------------------------- #
-file_type=${1} # Type of imputation  file
+file_type=${1} # Type of imputation file
 imputation_file=${2} # Path to imputation file
 sample_file=${3} # If .bgen doesn't have a header block, this file must have the sample ID information. Troubleshooting here: https://large-scale-gxe-methods.github.io/GEMShowcaseWorkspace
 pheno_file=${4} # Path to the phenotype file that includes information on outcomes, exposures, and covariates
@@ -73,7 +73,7 @@ covariates=${9} # List of covariates
 output_filename=${10} # GEM defaults the extension to 'gem.out'
 
 # ------------------------------------- #
-#  Starting script						#
+#  Starting script											#
 # ------------------------------------- #
 
 output=../results_for_upload/gem/${outcome}/${exposure}/
@@ -152,6 +152,25 @@ elif [[ ${file_type} = 'PGEN' ]]; then
 
 fi  	
 
+
+#### Merge output!!!
+
+for outcome in {HDLC LDLC TG}; do
+
+for exposure in {BMI AGE}; do
+
+for chr in {1..22}; do
+  results=/path/to/${outcome}_${exposure}.chr${chr}.GEM.out
+
+cat $results | sed '1d' >> ${outcome}_${exposure}.chrALL.GEM.out
+
+done
+
+cat ${outcome}_${exposure}.chrALL.GEM.out | sed '1s/^/SNPID\tRSID\tCHR\tPOS\tNon_Effect_Allele\tEffect_Allele\tN_Samples\tAF\tBeta_Marginal\trobust_SE_Beta_Marginal\tSE_Beta_Marginal\tBeta_G\tBeta_GxE\trobust_SE_Beta_G\trobust_SE_Beta_GxE\trobust_Cov_Beta_G_GxE\tSE_Beta_G\tSE_Beta_GxE\tCov_Beta_G_GxE\trobust_P_Value_Marginal\trobust_P_Value_Interaction\trobust_P_Value_Joint\tP_Value_Marginal\tP_Value_Interaction\tP_Value_Joint\n/'| gzip > ${outcome}_${exposure}.chrALL.GEM.out.gz
+
+done
+
+done
 
 
 ### excluude SNPs from outcome file based on... (align with MAGEE)
