6c1259841657c4c8efb15321dbad8f4bd329bbea
Jacqueline S. Dron <13053565+jsdron@users.noreply.github.com>
2023-05-24 13:34:54 -0400

    Script updates
    Updated the 4.1 workbook and list of dependancies

--- src/0.2_install-dependancies.sh
+++ src/0.2_install-dependancies.sh
@@ -4,7 +4,9 @@ BGZIP and tabix (0.2.6): http://samtools.sourceforge.net/tabix.shtml
 Bcftools (1.3.1): http://www.htslib.org/download
 qctools
 Vcftools (0.1.8): https://github.com/vcftools/vcftools
-PLINK1.9 (Plink2): https://www.cog-genomics.org/plink2
-PLINK 2.0
+PLINK1.9:
+PLINK2: https://www.cog-genomics.org/plink2
+### faster version PLINK
 GEM (1.4.5 or up): https://github.com/large-scale-gxe-methods/GEM 
 MAGEE
+R 
