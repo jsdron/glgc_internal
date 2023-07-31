4f570d607ece11360ea2b90f2badb79ecd170635
kyleyxw <20818794+kyleyxw@users.noreply.github.com>
2023-07-30 21:21:46 -0400

    Merge all MAGEE outputs

--- src/4.3.2_perform-GWIS_MAGEE.sh
+++ src/4.3.2_perform-GWIS_MAGEE.sh
@@ -11,18 +11,19 @@
 ## 																																
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Usage:																													
-## 			4.3.2_perform-GWIS_MAGEE.sh 	A 	B 	C 	D 
+## 			4.3.2_perform-GWIS_MAGEE.sh 	A 	B 	C 	D 	E
 ## 																																
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Input Parameters (* are required): 																							
 ##			*A (Type: String) = Path to null models													
 ##			*B (Type: String) = Path to genotype files (GDS format)
-##			*C (Type: String) = Exposure (interaction) variables (AGE/BMI)
-##			*D (Type: String) = Path to output files						
+##			*C (Type: String) = Outcome variables (LDLC/HDLC/TG)
+##			*D (Type: String) = Exposure (interaction) variables (AGE/BMI)
+##			*E (Type: String) = The prefix for all of the MAGEE output files.	Please use the naming convention detailed in XXXXXX. 	
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Example: 																														
 ## 			4.3.2_perform-GWIS_MAGEE.sh "/path/to/nullmodel/LDLC_ALLFAST_BMI_ALL_TOT_adult_case.glmmkin_nullmod.rds"
-##      "/path/to/genotype/chr1.gds" "BMI" "/path/to/output/LDLC_ALLFAST_BMI_ALL_TOT_adult_case"
+##      "/path/to/genotype/" "LDLC"  "BMI" "LDLC_ALLFAST_BMI_ALL_TOT_adult_case"
 ##						
 ##################################################################################################################################
 
@@ -32,18 +33,48 @@
 
 path_to_nullmodel=$1
 path_to_genotype=$2
-exposure=$3
-outfile=$4
+outcome=$3
+exposure=$4
+output_filename=$5
+
+output=../results_tmp/MAGEE/${outcome}/${exposure}/
+mkdir -p ${output}
 
 
 # ------------------------------------- #
 #  Starting script						#
 # ------------------------------------- #
 
-Rscript ../helper/4.3.2_perform-GWIS_MAGEE.R \
---path_to_nullmodel $path_to_nullmodel \
---path_to_genotype $path_to_genotype \
---exposure $exposure \
---outfile $outfile
+for chr in {1..22}; do
+    # Generate the specific input file path for each chromosome
+    geno_file="${path_to_genotype/chr$chr.gds}"
+    outfile="${output}${output_filename}/chr${chr}.magee.out"
 
+    # Call the R script for each chromosome-specific file
+    Rscript ../helper/4.3.2_perform-GWIS_MAGEE.R \
+        --path_to_nullmodel "$path_to_nullmodel" \
+        --path_to_genotype "$geno_file" \
+        --exposure "$exposure" \
+        --outfile "$outfile"
+done
+
+
+
+## Merge MAFEE output if chromosome files are separate
+for chr in {1..22}; do
+
+  results=${output}${output_filename}.chr${chr}.magee.out
+  cat $results | sed '1d' >> ${output}${output_filename}.chrALL.MAGEE.out
+
+done
+
+## This section should not be removed. It is important to note that it modifies the header
+output_final=../results_for_upload/MAGEE/
+mkdir -p ${output_final}
+
+cat ${output}${output_filename}.chrALL.MAGEE.out | sed '1s/^/SNPID\tCHR\tPOS\tNon_Effect_Allele\tEffect_Allele\tN_Samples\tAF\tBeta_Marginal\tSE_Beta_Marginal\tBeta_G\tBeta_GxE\tSE_Beta_G\tSE_Beta_GxE\tCov_Beta_G_GxE\tP_Value_Marginal\tP_Value_Interaction\tP_Value_Joint\n/'| gzip > ${output_final}${output_filename}.chrALL.MAGEE.out.gz
+
+rm ${output}${output_filename}.chrALL.MAGEE.out
+
+echo "Merged MAGEE output files successfully!"
 
