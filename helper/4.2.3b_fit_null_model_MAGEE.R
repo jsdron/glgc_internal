22a4175c1f90277678272b2883e4006ba96a3511
kyleyxw <20818794+kyleyxw@users.noreply.github.com>
2023-08-17 14:22:28 -0400

    Update 4.2.3b

--- helper/4.2.3b_fit_null_model_MAGEE.R
+++ helper/4.2.3b_fit_null_model_MAGEE.R
@@ -1,4 +1,5 @@
 ## Libraries
+rm(list = ls())
 library(data.table)
 library(GMMAT)
 library(GWASTools)
@@ -12,7 +13,7 @@ parser <- ArgumentParser()
 parser$add_argument("--pheno_file", type = "character", required = TRUE) # "/path/to/my/phenotype/fileName.csv"
 parser$add_argument("--outcome", type = "character", required = TRUE) # "LDLC"
 parser$add_argument("--grm", type = "character", required = TRUE) # "/path/to/grm/mypcrel.Rdata"
-parser$add_argument("--covariates", type = "character", action = "append", required = TRUE) # "sex age PC1 PC2 PC3 PC4 PC5"
+parser$add_argument("--covariates", type = "character", nargs = "*", required = TRUE) # "sex age PC1 PC2 PC3 PC4 PC5"
 parser$add_argument("--outfile", type = "character", required = TRUE) # "/path/to/nullmodel/LDLC_ALLFAST_BMI_ALL_TOT_adult_case.glmmkin_nullmod.rds"
 
 ## Parse arguments 
@@ -22,7 +23,7 @@ pheno_file <- args$pheno_file
 
 outcome <- args$outcome
 
-grm <- args$path_to_genotype 
+grm <- args$grm 
 
 ### Covariates
 if (!is.null(args$covariates)) {
@@ -35,16 +36,23 @@ if (!is.null(args$covariates)) {
 
 outfile <- args$outfile
 
+print(paste("Phenotype File Path: ", pheno_file))
+print(paste("Outcome: ", outcome))
+print(paste("GRM File Path: ", grm))
+print(paste("Covariates: ", paste(covariates, collapse = ", ")))
+print(paste("Output File Path: ", outfile))
+
+
 ## File with outcome, exposure, and covariate information
 # Sample ID column header should be labelled as “sample.id”
 pheno <- fread(pheno_file)
 
 ## GRM generated from GENESIS or you can use your own GRM and convert it to a matrix. 
 mypcrel <- GWASTools::getobj(grm)
-kin.mat <- pcrelateToMatrix(mypcrel)
 
-## Subset GRM based on the sample ID if needed: e.g. kin.mat <- kin.mat[sample.id,sample.id]
-kin.mat <- kin.mat[sample.id,sample.id]
+## Subset GRM based on the sample ID if needed: e.g. sample.include = pheno$sample.id
+kin.mat <- pcrelateToMatrix(mypcrel,sample.include = pheno$sample.id)
+
 
 ## Generate the null model for LDLC
 # Adjust the variable names to reflect column names in pheno
