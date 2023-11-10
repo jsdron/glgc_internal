7db21af592f16861d7cd65fa0e0cfa5ec55f2a6a
kyleyxw <20818794+kyleyxw@users.noreply.github.com>
2023-11-10 11:17:44 -0500

    Update Null Model

--- src/4.2.3b_fit_null_model_MAGEE.sh
+++ src/4.2.3b_fit_null_model_MAGEE.sh
@@ -6,24 +6,25 @@
 ## 	Description: This is a wrapper that simplifies the process of running an R script "4.2.4_fit_null_model_MAGEE.R" 
 ##               by providing the necessary command-line arguments. 				
 ## 	Authors: Yuxuan Wang <yxw@bu.edu>
-## 	Date: 2023-08-17																											
-## 	Version: 1.0																												
+## 	Date: 2023-11-01																											
+## 	Version: 1.1																												
 ## 																																
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Usage:																													
-## 			4.2.3b_fit_null_model_MAGEE.sh 	A 	B 	C 	D 	E
+## 			4.2.3b_fit_null_model_MAGEE.sh 	A 	B 	C 	D 	E   F
 ## 																																
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Input Parameters (* are required): 																							
 ##			*A (Type: String) = Path to the phenotype file, including file prefix and extension.  								
 ##			*B (Type: String) = Outcome. 
-##			*C (Type: String) = Path to GRM. 													
-##			*D (Type: String) = Covariates as a space-separated list.
-##			*E (Type: String) = Path to the output file, including file prefix and extension. 
+##			*C (Type: String) = Path to GRM. 	
+##			*D (Type: String) = Exposure. 
+##			*E (Type: String) = Covariates as a space-separated list.
+##			*F (Type: String) = Path to the output file, including file prefix and extension. 
 ## ---------------------------------------------------------------------------------------------------------------------------- 
 ## 	Example: 																														
-## 			4.2.3b_fit_null_model_MAGEE.sh "/path/to/my/phenotype/fileName.csv" "LDLC" "/path/to/grm/mypcrel.Rdata" 
-##			"sex,age,PC1,PC2,PC3,PC4,PC5" "/path/to/nullmodel/LDLC_ALLFAST_BMI_ALL_TOT_adult_case.glmmkin_nullmod.rds"
+## 			4.2.3b_fit_null_model_MAGEE.sh "/path/to/my/phenotype/fileName.csv" "LDLC" "/path/to/grm/mypcrel.Rdata" "age"
+##			"sex,PC1,PC2,PC3,PC4,PC5" "/path/to/nullmodel/LDLC_ALLFAST_AGE_ALL_TOT_adult_case.glmmkin_nullmod.rds"
 ## 
 ##################################################################################################################################
 
